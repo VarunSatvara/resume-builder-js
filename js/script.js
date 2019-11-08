a7169803c2282784a1f446c9f9455c0cefee664c
varun satvara <varunsatvara1997@gmail.com>
2019-11-08 18:02:18 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function edited() {
+    document.getElementsByTagName('h1')[0].contentEditable = "true";
+    document.getElementsByTagName('small')[0].contentEditable = "true";
+}
+
+function editednot() {
+    document.getElementsByTagName('h1')[0].contentEditable = "false";
+    document.getElementsByTagName('small')[0].contentEditable = 'false';
+}
