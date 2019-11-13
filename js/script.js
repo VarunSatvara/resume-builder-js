5a89c29f6579b0ff99bc9c2ec2477063a22e7788
varun satvara <varunsatvara1997@gmail.com>
2019-11-13 15:59:41 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function edited() {
-    document.getElementsByTagName('h1')[0].contentEditable = "true";
+    document.getElementsByTagName('p')[0].contentEditable = "true";
     document.getElementsByTagName('small')[0].contentEditable = "true";
+    var i;
+    for (i = 0; i < 6; i++) {
+        document.getElementsByTagName('h1')[i].contentEditable = "true";
+    }
+
+    var count;
+    for (count = 0; count < 5; count++) {
+        document.getElementsByTagName('ul')[count].contentEditable = "true";
+    }
+
+
 }
 
 function editednot() {
-    document.getElementsByTagName('h1')[0].contentEditable = "false";
+    document.getElementsByTagName('p')[0].contentEditable = "false";
     document.getElementsByTagName('small')[0].contentEditable = 'false';
+    var i;
+    for (i = 0; i < 6; i++) {
+        document.getElementsByTagName('h1')[i].contentEditable = "false";
+    }
+
+    var count;
+    for (count = 0; count < 5; count++) {
+        document.getElementsByTagName('ul')[count].contentEditable = "false";
+    }
 }
